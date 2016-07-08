780264772a2276525a2e1e95a4e124168e733be9
Volodymyr Honcharko <vhoncharko@gmail.com>
2016-07-08 15:05:47 +0300

    hw fixed

--- js/h3-4/script.js
+++ js/h3-4/script.js
@@ -1,179 +1,61 @@
 var test = {
-    wrapper: function() {
-        var parent = document.getElementsByTagName("body")[0];
-        var element = document.createElement("div");
-        element.classList.add("wrapper");
-        parent.appendChild(element);
+    main: "Тест по программированию",
+    questions : ["1. Вопрос №1", "2. Вопрос №2", "3. Вопрос №3"],
+    answears: [["Вариант ответа №1", "Вариант ответа №2", "Вариант ответа №3"],
+               ["Вариант ответа №1", "Вариант ответа №2", "Вариант ответа №3"],
+               ["Вариант ответа №1", "Вариант ответа №2", "Вариант ответа №3"]],
+    text: "Проверить мои результаты",
+    title: function(main){
+        var div;
+        div = document.createElement("h3");
+        div.innerHTML = this.main;
+        div.style.textAlign = "center";
+        document.body.appendChild(div);
     },
-    box: function() {
-        var par = document.createElement("div");
-        par.classList.add("box");
-        par.style.width = "300px";
-        par.style.display = "block";
-        var wrapper = document.querySelectorAll(".wrapper");
-        wrapper[0].appendChild(par);
-    },
-    main: function(){
-        var text = document.createElement("h3");
-        text.innerHTML = 'Тест по программированию';
-        text.style.textAlign = "center";
-        var wrapper = document.querySelectorAll(".wrapper");
-        wrapper[0].appendChild(text);
+    counter: function() {
+        var c = this.questions.length;
+        var ans, ul, li, p, input;
+        for (var i = 0; i < c; i++) {
+            p = document.createElement("p");
+            p.innerHTML = this.questions[i];
+            document.body.appendChild(p);
+            p.style.marginLeft = "80px";
+            ul = document.createElement("ul");
+            p.appendChild(ul);
+            ans = this.answears[i].length;
+            for (var q = 0; q < ans; q++) {
+                li = document.createElement("li");
+                li.style.listStyle = "none";
+                input = document.createElement("input");
+                input.setAttribute("type","checkbox");
+                ul.appendChild(input);
+                input.style.float = "left";
+                input.style.paddingLeft = "100px";
+                li.innerHTML = this.answears[i][q];
+                ul.appendChild(li);
+
+            }
+
+        }
 
     },
-    question: function(){
-        var num = document.createElement("p");
-        num.innerHTML = "1. Вопрос №1";
-        var box = document.querySelectorAll(".box");
-        num.style.marginLeft = "80px";
-        box[0].appendChild(num);
-    },
-    input: function(){
-        var input = document.createElement("input");
-        input.setAttribute("type", "checkbox");
-        input.setAttribute("id","answearFirst");
-        var label = document.createElement("label");
-        label.setAttribute("for", "answearFirst");
-        label.innerHTML = "Вариант ответа №1";
-        input.style.marginLeft = "100px";
-        input.style.display = "block";
-        input.style.float = "left";
-        label.style.display = "block";
-        label.style.float = "left";
-        var box = document.querySelectorAll(".box");
-        box[0].appendChild(input);
-        box[0].appendChild(label);
-
-        input = document.createElement("input");
-        input.setAttribute("type", "checkbox");
-        input.setAttribute("id","answearSecond");
-        label = document.createElement("label");
-        label.setAttribute("for", "answearSecond");
-        label.innerHTML = "Вариант ответа №2";
-        input.style.marginLeft = "100px";
-        box = document.querySelectorAll(".box");
-        box[0].appendChild(input);
-        box[0].appendChild(label);
-
-        input = document.createElement("input");
-        input.setAttribute("type", "checkbox");
-        input.setAttribute("id","answearThird");
-        label = document.createElement("label");
-        label.setAttribute("for", "answearThird");
-        label.innerHTML = "Вариант ответа №3";
-        input.style.marginLeft = "100px";
-        box = document.querySelectorAll(".box");
-        box[0].appendChild(input);
-        box[0].appendChild(label);
-
-        num = document.createElement("p");
-        num.innerHTML = "2. Вопрос №2";
-        box = document.querySelectorAll(".box");
-        num.style.marginLeft = "80px";
-        box[0].appendChild(num);
-
-        input = document.createElement("input");
-        input.setAttribute("type", "checkbox");
-        input.setAttribute("id","answearFour");
-        label = document.createElement("label");
-        label.setAttribute("for", "answearFour");
-        label.innerHTML = "Вариант ответа №1";
-        input.style.marginLeft = "100px";
-        input.style.display = "block";
-        input.style.float = "left";
-        label.style.display = "block";
-        label.style.float = "left";
-        box = document.querySelectorAll(".box");
-        box[0].appendChild(input);
-        box[0].appendChild(label);
-
-        input = document.createElement("input");
-        input.setAttribute("type", "checkbox");
-        input.setAttribute("id","answearFive");
-        label = document.createElement("label");
-        label.setAttribute("for", "answearFive");
-        label.innerHTML = "Вариант ответа №2";
-        input.style.marginLeft = "100px";
-        box = document.querySelectorAll(".box");
-        box[0].appendChild(input);
-        box[0].appendChild(label);
-
-        input = document.createElement("input");
-        input.setAttribute("type", "checkbox");
-        input.setAttribute("id","answearSix");
-        label = document.createElement("label");
-        label.setAttribute("for", "answearSix");
-        label.innerHTML = "Вариант ответа №3";
-        input.style.marginLeft = "100px";
-        box = document.querySelectorAll(".box");
-        box[0].appendChild(input);
-        box[0].appendChild(label);
-
-        num = document.createElement("p");
-        num.innerHTML = "3. Вопрос №3";
-        box = document.querySelectorAll(".box");
-        num.style.marginLeft = "80px";
-        box[0].appendChild(num);
-
-        input = document.createElement("input");
-        input.setAttribute("type", "checkbox");
-        input.setAttribute("id","answearSeven");
-        label = document.createElement("label");
-        label.setAttribute("for", "answearSeven");
-        label.innerHTML = "Вариант ответа №1";
-        input.style.marginLeft = "100px";
-        input.style.display = "block";
-        input.style.float = "left";
-        label.style.display = "block";
-        label.style.float = "left";
-        box = document.querySelectorAll(".box");
-        box[0].appendChild(input);
-        box[0].appendChild(label);
-
-        input = document.createElement("input");
-        input.setAttribute("type", "checkbox");
-        input.setAttribute("id","answearEight");
-        label = document.createElement("label");
-        label.setAttribute("for", "answearEight");
-        label.innerHTML = "Вариант ответа №2";
-        input.style.marginLeft = "100px";
-        box = document.querySelectorAll(".box");
-        box[0].appendChild(input);
-        box[0].appendChild(label);
-
-        input = document.createElement("input");
-        input.setAttribute("type", "checkbox");
-        input.setAttribute("id","answearNine");
-        label = document.createElement("label");
-        label.setAttribute("for", "answearNine");
-        label.innerHTML = "Вариант ответа №3";
-        input.style.marginLeft = "100px";
-        box = document.querySelectorAll(".box");
-        box[0].appendChild(input);
-        box[0].appendChild(label);
-    },
-    button: function(){
-        var submit = document.createElement("input");
-        submit.setAttribute("type", "submit");
-        submit.setAttribute("value","Проверить мои результаты");
-        submit.innerHTML = "Проверить мои результаты";
-        submit.style.display = "block";
-        submit.style.marginLeft = "295px";
-        submit.style.width = "300px";
-        submit.style.height = "50px";
-        submit.style.color = "black";
-        submit.style.fontSize = "14px";
-        submit.style.background = "#9fc1f3";
-        submit.style.border = "1px solid black";
-        var wrapper = document.querySelectorAll(".wrapper");
-        wrapper[0].appendChild(submit);
-    }
+            button: function() {
+                var form, button;
+                form = document.createElement("form");
+                document.body.appendChild(form);
+                form.style.textAlign = "center";
+                button = document.createElement("button");
+                button.innerHTML = this.text;
+                button.style.width = "300px";
+                button.style.height = "50px";
+                button.style.fontSize = "14px";
+                button.style.background = "#9fc1f3";
+                button.style.border = "1px solid black";
+                button.setAttribute("formaction", "#");
+                form.appendChild(button);
+            }
 };
-test.wrapper();
-test.main();
-test.box();
-test.question();
-test.input();
+
+test.title();
+test.counter();
 test.button();
-
-
