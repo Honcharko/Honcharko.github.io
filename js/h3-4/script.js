6cd5f988fa68cd337ad3d988a0d205b11b218319
Volodymyr Honcharko <vhoncharko@gmail.com>
2016-07-08 16:39:36 +0300

    hw fixed_2

--- js/h3-4/script.js
+++ js/h3-4/script.js
@@ -1,9 +1,6 @@
 var test = {
     main: "Тест по программированию",
-    questions : ["1. Вопрос №1", "2. Вопрос №2", "3. Вопрос №3"],
-    answears: [["Вариант ответа №1", "Вариант ответа №2", "Вариант ответа №3"],
-               ["Вариант ответа №1", "Вариант ответа №2", "Вариант ответа №3"],
-               ["Вариант ответа №1", "Вариант ответа №2", "Вариант ответа №3"]],
+    questions : ["№1", "№2", "№3"],
     text: "Проверить мои результаты",
     title: function(main){
         var div;
@@ -14,16 +11,15 @@ var test = {
     },
     counter: function() {
         var c = this.questions.length;
-        var ans, ul, li, p, input;
+        var ul, li, p, input;
         for (var i = 0; i < c; i++) {
             p = document.createElement("p");
-            p.innerHTML = this.questions[i];
+            p.innerHTML = Number(i+1)+ ".Вопрос " +this.questions[i];
             document.body.appendChild(p);
             p.style.marginLeft = "80px";
             ul = document.createElement("ul");
             p.appendChild(ul);
-            ans = this.answears[i].length;
-            for (var q = 0; q < ans; q++) {
+            for (var q = 0; q < c; q++) {
                 li = document.createElement("li");
                 li.style.listStyle = "none";
                 input = document.createElement("input");
@@ -31,7 +27,7 @@ var test = {
                 ul.appendChild(input);
                 input.style.float = "left";
                 input.style.paddingLeft = "100px";
-                li.innerHTML = this.answears[i][q];
+                li.innerHTML = "Вариант ответа " + this.questions[q];
                 ul.appendChild(li);
 
             }
@@ -39,21 +35,21 @@ var test = {
         }
 
     },
-            button: function() {
-                var form, button;
-                form = document.createElement("form");
-                document.body.appendChild(form);
-                form.style.textAlign = "center";
-                button = document.createElement("button");
-                button.innerHTML = this.text;
-                button.style.width = "300px";
-                button.style.height = "50px";
-                button.style.fontSize = "14px";
-                button.style.background = "#9fc1f3";
-                button.style.border = "1px solid black";
-                button.setAttribute("formaction", "#");
-                form.appendChild(button);
-            }
+    button: function() {
+        var form, button;
+        form = document.createElement("form");
+        document.body.appendChild(form);
+        form.style.textAlign = "center";
+        button = document.createElement("button");
+        button.innerHTML = this.text;
+        button.style.width = "300px";
+        button.style.height = "50px";
+        button.style.fontSize = "14px";
+        button.style.background = "#9fc1f3";
+        button.style.border = "1px solid black";
+        button.setAttribute("formaction", "#");
+        form.appendChild(button);
+    }
 };
 
 test.title();
