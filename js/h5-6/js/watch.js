1ff713efb9bbd004496ecf5552d55cb4593cf1bc
Volodymyr Honcharko <vhoncharko@gmail.com>
2016-07-18 17:04:53 +0300

    test final

--- js/h5-6/js/watch.js
+++ js/h5-6/js/watch.js
@@ -14,7 +14,9 @@ function startClick(){
             var sec = Math.floor(count / 100) % 60;
             if(sec < 10){sec ='0' + sec;}
             var min = Math.floor(count / 6000) % 60;
+            if(min < 10){min ='0' + min;}
             var hours = Math.floor(count / 360000) % 24;
+            if(hours < 10){hours ='0' + hours;}
             var str = hours + ':' + min + ':' + sec + ':' + msec;
             countDisp.innerHTML = str;
         },10);
@@ -25,6 +27,7 @@ function startClick(){
         start.removeEventListener('click', startClick);
         var pause = document.getElementById('start');
         pause.addEventListener('click', pauseClick);
+        pause.style.backgroundColor = '#3b83bc';
     }
 }
 
@@ -34,6 +37,7 @@ function pauseClick(){
     start.innerHTML = 'Cont...';
     if(start.innerHTML = 'Cont...'){
       var cont = document.getElementById('start');
+        cont.style.backgroundColor = 'rgb(28, 184, 65)';
         cont.addEventListener('click', startClick);
     }
 }
@@ -44,4 +48,6 @@ function clearClick(){
     count = 0;
     countDisp.innerHTML = '<span class="time">00:00:00</span><span class="clock">0</span>';
     start.innerHTML = 'Start';
+    start.addEventListener('click', startClick);
+
 }
