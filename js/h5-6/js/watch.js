445831fb5894d8b00fb9e424c517f9578cc90d89
Volodymyr Honcharko <vhoncharko@gmail.com>
2016-07-18 23:24:27 +0300

    fixed 1

--- js/h5-6/js/watch.js
+++ js/h5-6/js/watch.js
@@ -10,15 +10,19 @@ function startClick(){
     if(lap == false){
         timerId = setInterval(function(){
             count++;
-            var msec = count % 100;
+            var msec = Math.floor(count % 100);
             var sec = Math.floor(count / 100) % 60;
             if(sec < 10){sec ='0' + sec;}
             var min = Math.floor(count / 6000) % 60;
             if(min < 10){min ='0' + min;}
             var hours = Math.floor(count / 360000) % 24;
             if(hours < 10){hours ='0' + hours;}
-            var str = hours + ':' + min + ':' + sec + ':' + msec;
-            countDisp.innerHTML = str;
+            var str = hours + ':' + min + ':' + sec;
+            var x = document.getElementsByClassName('time')[0];
+            var y = document.getElementsByClassName('clock')[0];
+            console.log(y);
+            x.innerHTML = str;
+            y.innerHTML = msec;
         },10);
         lap = true;
     }
@@ -30,6 +34,7 @@ function startClick(){
         pause.style.backgroundColor = '#3b83bc';
     }
 }
+
 
 function pauseClick(){
     clearInterval(timerId);
