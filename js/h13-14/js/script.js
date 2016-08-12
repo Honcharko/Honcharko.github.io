6a96477f07b35f6377c0847ad8606ab33955d430
Volodymyr Honcharko <vhoncharko@gmail.com>
2016-08-12 12:46:17 +0300

    hw 13-14 test

--- js/h13-14/js/script.js
+++ js/h13-14/js/script.js
@@ -1,42 +1,36 @@
 var test = {
-    main: "Тест по программированию",
-    questions : ["Ваше имя", "Ваша фамилия", "Ваш возраст"],
+    main: "Тест",
+    questions : ["Ваше имя :", "Ваша фамилия :", "Ваш возраст :"],
     answers : [["Петя", "Вася"],["Иванов", "Петров"],["25", "22"]],
     right: [1, 1, 1]
 };
 
-//function () {
-//    var c = this.questions.length;
-//    var w = this.answers.length;
-//    var obj = {};
-//    for (var i = 0; i < c; i++) {
-//        obj[this.questions[i]] = this.answers[i];
-//        for (var q = 0; q < w; q++) {
-//            obj[this.questions[q]] = this.answers[q];
-//        }
-//    }
-//    return obj;
-//}
         var jsonTest = JSON.stringify(test);
         localStorage.setItem('testing', jsonTest);
 
         jsonTest = localStorage.getItem('testing');
         jsonTest = JSON.parse(jsonTest);
-console.log(jsonTest);
 
-//    localStorage.setItem('testing', JSON.stringify(test.counter()));
-//    var testing = localStorage.getItem('testing');
-//
-//    testing = JSON.parse(testing);
-//
-//
-//$(function(){
-//    var html = $('.tmpl').html();
-//    var content = tmpl(html, {
-//        data: testing
-//    });
-//    //console.log(content);
-//    $('body').append(content);
-//});
+$(function(){
+
+    var html = $('.tmpl').html();
+    var content = tmpl(html, {
+        data: jsonTest
+    });
+    $('body').append(content);
+
+    //submit
+
+    $('.submit').on('click', function(){
+
+        if($('#a00 input:checked').length == 1){
+           console.log('ok');
+        }
+
+
+    });
+
+
+});
 
 
