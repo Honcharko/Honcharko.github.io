faaeb625e5c642a3ef51e5d265f6fc1fc313c15a
Volodymyr Honcharko <vhoncharko@gmail.com>
2016-08-15 16:12:17 +0300

    hw 13-14 final

--- js/h13-14/js/script.js
+++ js/h13-14/js/script.js
@@ -1,17 +1,18 @@
+    'use strict';
+$(function(){
 var test = {
     main: "Тест",
     questions : ["Ваше имя :", "Ваша фамилия :", "Ваш возраст :"],
-    answers : [["Петя", "Вася"],["Иванов", "Петров"],["25", "22"]],
-    right: [1, 1, 1]
+    answers : [["Петя", "Вася"],["Иванов", "Петров"],["25", "22"]]
 };
 
         var jsonTest = JSON.stringify(test);
         localStorage.setItem('testing', jsonTest);
 
-        jsonTest = localStorage.getItem('testing');
-        jsonTest = JSON.parse(jsonTest);
 
-$(function(){
+    jsonTest = localStorage.getItem('testing');
+    jsonTest = JSON.parse(jsonTest);
+
 
     var html = $('.tmpl').html();
     var content = tmpl(html, {
@@ -19,17 +20,52 @@ $(function(){
     });
     $('body').append(content);
 
+    var $submit = $('.submit')[0];
+
     //submit
+    $($submit).on('click', function(e){
+        var $cnt = 0;
 
-    $('.submit').on('click', function(){
-
-        if($('#a00 input:checked').length == 1){
-           console.log('ok');
+        e.preventDefault();
+        if($('#a00 input:checked').length==1 && $('#а01 input:checked').length==0){
+            $cnt++;
+        }else{
+            $cnt = 0;
+        }
+        if($('#a10 input:checked').length==0 && $('#a11 input:checked').length==1){
+            $cnt++;
+        }
+        if($('#a20 input:checked').length==1 && $('#а21 input:checked').length==0){
+            $cnt++;
         }
 
+        //modal window
+        var window = function window(){
+
+
+            var $modal = $('<div class = "window"><span> У вас '+ $cnt +' правильных (-й) ответов (-т)</span></div>');
+            var $button = $('<button class="reset">OK</button>');
+            var $overlay = $('<div class= "window-overlay">');
+
+
+            $('body').append($overlay);
+            $('body').append($modal);
+            $($modal).append($button);
+
+            var $reset = $('.reset')[0];
+
+            function reset(){
+                $modal.remove();
+                $overlay.remove();
+                $('.quest')[0].reset();
+            }
+
+            $reset.addEventListener('click', reset);
+        };
+
+        window();
 
     });
-
 
 });
 
